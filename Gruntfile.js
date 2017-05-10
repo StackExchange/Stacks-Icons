7d04db3ff92d72d749adc01b835e473a482754c7
Aaron Shekey <aaron@aaronshekey.com>
2017-05-10 13:20:03 -0500

    Bake attributes into our SVGs

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,18 @@ module.exports = function(grunt) {
       options: {
   			plugins: [
   				{
+            // Unfortunately, adding attributes doesn't play well with removing
+            // them. For reasons™, we can't rely on our Svg.cs helper to add
+            // them for us. We'll use a find and replace after SVGmin on each
+            // of these built SVGs.
+            //
+            // addAttributesToSVGElement: {
+            //   attributes: ['class="icon"', 'role="icon"']
+            // },
             removeAttrs: {
   						attrs: ['xmlns', 'fill-rule']
   					},
-            collapseGroups: true
+            collapseGroups: true,
   				}
   			]
   		},
@@ -22,13 +30,30 @@ module.exports = function(grunt) {
           dest: 'build',
         }]
   		}
-  	}
+  	},
+    'string-replace': {
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'build/',
+          src: '**/*',
+          dest: 'build/'
+        }],
+        options: {
+          replacements: [{
+            pattern: '<svg',
+            replacement: '<svg role="icon" class="icon"'
+          }]
+        }
+      }
+    }
   });
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-svgmin');
+  grunt.loadNpmTasks('grunt-string-replace');
 
   // Default task(s).
-  grunt.registerTask('default', ['clean','svgmin']);
+  grunt.registerTask('default', ['clean','svgmin', 'string-replace']);
 };
