567f7eb266600a326a051f4b7170928a57ee72c1
Aaron Shekey <aaron@aaronshekey.com>
2017-06-23 13:07:20 -0500

    Refactor grunt a bit

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,20 +4,19 @@ module.exports = function(grunt) {
     clean: ["build"],
     svgmin: {
       options: {
-        plugins: [{
-          // Unfortunately, adding attributes doesn't play well with removing
-          // them. For reasons™, we can't rely on our Svg.cs helper to add
-          // them for us. We'll use a find and replace after SVGmin on each
-          // of these built SVGs.
-          //
-          // addAttributesToSVGElement: {
-          //   attributes: ['class="icon"', 'role="icon"']
-          // },
-          removeAttrs: {
-            attrs: ['xmlns', 'fill-rule']
-          },
-          collapseGroups: true,
-        }]
+        plugins: [
+          {
+            collapseGroups: true,
+          }, {
+            removeTitle: true,
+          }, {
+            removeUselessStrokeAndFill: true,
+          }, {
+            removeAttrs: {
+              attrs: ['xmlns', 'fill-rule']
+            }
+          }
+        ]
       },
       build: {
         files: [{
@@ -41,14 +40,9 @@ module.exports = function(grunt) {
             pattern: '<svg',
             replacement: '<svg role="icon" class="svg-icon icon@@__TARGET_FILENAME__"'
           }, {
-            pattern: /<title>.*<\/title>/,
-            replacement: ''
-          },
-          {
             pattern: ' fill="#000"',
             replacement: ''
-          },
-          {
+          }, {
             pattern: ' fill="none"',
             replacement: ''
           }]
