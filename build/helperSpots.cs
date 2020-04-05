a06e0bf0d9fa1cdb538b842f175b41315fc076a3
Aaron Shekey <aaron@aaronshekey.com>
2020-04-05 12:58:31 -0500

    Refine build and document what each grunt task does
    My future self will thank me.

---
+++ build/helperSpots.cs
@@ -0,0 +1,7 @@
+public static SvgImage BellMd { get; } = GetImage();
+public static SvgImage LocationMd { get; } = GetImage();
+public static SvgImage MailMd { get; } = GetImage();
+public static SvgImage SearchMd { get; } = GetImage();
+public static SvgImage ShieldMd { get; } = GetImage();
+public static SvgImage TrashMd { get; } = GetImage();
+public static SvgImage WaveMd { get; } = GetImage();
