6c69391f791c73e4ad9bb9fa951bb32104c2f128
Aaron Shekey <aaron@aaronshekey.com>
2020-04-29 13:17:49 -0500

    Refinements on Wave

--- build/Spots.cs
+++ build/Spots.cs
@@ -1,16 +1,6 @@
 public enum Icons
 {
-    AchievementsLg,
-    AchievementsMd,
-    AchievementsXL,
-    BellMd,
-    LocationMd,
-    MailMd,
-    SearchMd,
-    ShieldMd,
-    TrashMd,
     Wave,
     WaveLg,
-    WaveMd,
     WaveXL,
 }
