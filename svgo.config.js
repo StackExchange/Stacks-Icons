c757c6539742db5da3096baf1476535ed16360b5
David Longworth <dlongworth@stackoverflow.com>
2021-04-07 18:50:42 +0100

    use extendDefaultPlugins

--- svgo.config.js
+++ svgo.config.js
@@ -1,46 +1,42 @@
+const { extendDefaultPlugins } = require('svgo')
+
 module.exports = {
-    multipass: true,
-    plugins: [
-        {
-            name: "convertPathData",
-            params: {
-                floatPrecision: 2,
-                transformPrecision: 4
-            }
-        },
-        {
-            name: "cleanupNumericValues",
-            params: {
-                floatPrecision: 2
-            }
-        },
-        {
-            name: "removeAttrs",
-            params: {
-                attrs: "(fill-rule|clip-rule)"
-            }
-        },
-        {
-            name: "removeViewBox",
-            active: false
-        },
-        {
-            name: "removeXMLNS",
-            active: true
-        },
-        {
-            name: "mergePaths",
-            params: {
-                force: true
-            }
-        },
-        {
-            name: "collapseGroups",
-            active: true
-        },
-        {
-            name: "removeUselessStrokeAndFill",
-            active: true
-        }
-    ]
+  multipass: true,
+  plugins: extendDefaultPlugins([
+    {
+      name: 'convertPathData',
+      params: {
+        floatPrecision: 2,
+        transformPrecision: 4,
+        noSpaceAfterFlags: true
+      }
+    },
+    {
+      name: 'cleanupNumericValues',
+      params: {
+        floatPrecision: 2
+      }
+    },
+    {
+      name: 'removeAttrs',
+      params: {
+        attrs: '(fill-rule|clip-rule)'
+      }
+    },
+    {
+      name: 'removeViewBox',
+      active: false
+    },
+    {
+      name: 'removeXMLNS',
+      active: true
+    },
+    {
+      name: 'mergePaths',
+      params: {
+        force: true,
+        noSpaceAfterFlags: true
+      }
+    }
+  ])
 }
