9b33e7e6b07196d5ca50c6cf9207b8198f8aa6a9
Ben Kelly <b-kelly@users.noreply.github.com>
2022-12-08 17:49:02 -0500

    add script to bump nuget version when np version is bumped

---
+++ scripts/bumpVersions.ts
@@ -0,0 +1,28 @@
+import { promisify } from "util";
+import { exec } from "child_process";
+import { error, success } from "./utils.js";
+import packageJson from "../package.json" assert { type: "json" };
+import { readFile, writeFile } from "fs/promises";
+
+const execAsync = promisify(exec);
+
+(async () => {
+  const version = packageJson.version;
+  const path = "nuget/src/StackExchange.StacksIcons.csproj";
+
+  var file = await readFile(path, "utf-8");
+
+  file = file.replace(
+    /<Version>([\d.]+?)<\/Version>/,
+    `<Version>${version}</Version>`
+  );
+
+  await writeFile(path, file, "utf-8");
+
+  await execAsync("git add " + path);
+
+  success(`Wrote version ${version} to csproj`);
+})().catch((e) => {
+  error(e);
+  process.exit(1);
+});
