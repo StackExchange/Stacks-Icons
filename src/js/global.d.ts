86a2d891785105e0d9d35030541951b95e0ab860
David Longworth <djl@sent.com>
2025-03-25 12:55:51 +0000

    remove ignore

--- src/js/global.d.ts
+++ src/js/global.d.ts
@@ -1,7 +1,4 @@
-// @ts-ignore: these only exist at build time
 declare module "@stackoverflow/stacks-icons" {
-    // @ts-ignore
     export * as Icons from "@stackoverflow/stacks-icons/icons";
-    // @ts-ignore
     export * as Spots from "@stackoverflow/stacks-icons/spots";
 }
