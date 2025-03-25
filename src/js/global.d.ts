149846382e089a05eabb77aa2193fadd943d70c7
David Longworth <djl@sent.com>
2025-03-25 12:54:18 +0000

    add missing curly

--- src/js/global.d.ts
+++ src/js/global.d.ts
@@ -1,5 +1,5 @@
 // @ts-ignore: these only exist at build time
-declare module "@stackoverflow/stacks-icons" 
+declare module "@stackoverflow/stacks-icons" {
     // @ts-ignore
     export * as Icons from "@stackoverflow/stacks-icons/icons";
     // @ts-ignore
